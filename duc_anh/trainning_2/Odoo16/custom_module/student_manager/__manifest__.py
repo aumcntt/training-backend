8ffe85918a6c9653e5c455a11c97a83d85eb6f99
omg2468 <115636091+omg2468@users.noreply.github.com>
2025-02-21 08:22:31 +0700

    update for odoo

---
+++ duc_anh/trainning_2/Odoo16/custom_module/student_manager/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'student_manager',
+    'version': '1.0',
+    'category': 'Student',
+    'summary': 'A custom module for learning purposes',
+    'description': 'This module is a demonstration of creating a custom Odoo module.',
+    'author': 'Your Name',
+    'website': 'https://www.yourwebsite.com',
+    'depends': [],
+    'data': [
+        'views/student_view.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ duc_anh/trainning_2/Odoo16/custom_module/student_manager/models/student_model.py
@@ -0,0 +1,24 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+class Student(models.Model):
+    _name = 'student.student'
+    _description = 'Student Information'
+
+    name = fields.Char(string='Tên Sinh Viên', required=True)
+    student_code = fields.Char(string='Mã Sinh Viên', required=True)
+    display_name = fields.Char(string='Tên Hiển Thị', readonly=True , compute='_compute_display_name', store=False)
+    birth_date = fields.Date(string='Ngày Sinh')
+    email = fields.Char(string='Email')
+    phone = fields.Char(string='Số Điện Thoại')
+
+    @api.depends('name', 'student_code')
+    def _compute_display_name(self):
+        for student in self:
+            student.display_name = f'{student.name}-{student.student_code}'
+
+    @api.constrains('student_code')
+    def _check_unique_student_id(self):
+        for record in self:
+            if self.search_count([('student_code', '=', record.student_code)]) > 1:
+                raise ValidationError("Mã sinh viên đã tồn tại, vui lòng nhập mã sinh viên khác!")
